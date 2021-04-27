882ffc8344c2109016c6ebbb88c8c867a3d9073d
narcis_formagiu <narcis.formagiu@elision.eu>
2021-04-27 10:37:23 +0200

    Added new field for the patient object

--- src/main/java/org/n/iotbackend/model/datamodel/FullPatientObject.java
+++ src/main/java/org/n/iotbackend/model/datamodel/FullPatientObject.java
@@ -1,13 +1,15 @@
 package org.n.iotbackend.model.datamodel;
 
 import javax.persistence.Entity;
+import java.util.Date;
 
 @Entity
 public class FullPatientObject extends SimplePatient{
 
     private String firstName;
     private String secondName;
-    // TODO: add the rest of the data needed for the record.
+    private Date birthdate;
+// TODO: add the rest of the data needed for the record.
 
     public String getFirstName() {
         return firstName;
@@ -23,5 +25,13 @@ public class FullPatientObject extends SimplePatient{
 
     public void setSecondName(String secondName) {
         this.secondName = secondName;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
     }
 }
